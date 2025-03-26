188d22e2de77fa3b49ef03ac295470299490fe38
Tomer Geva <gevatomer66@gmail.com>
2025-03-26 19:04:56 +0200

    created cig++ server using spring initializer

---
+++ Cigpp/src/main/java/bnb/cigpp/CigppApplication.java
@@ -0,0 +1,13 @@
+package bnb.cigpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CigppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CigppApplication.class, args);
+	}
+
+}
---
+++ Cigpp/src/test/java/bnb/cigpp/CigppApplicationTests.java
@@ -0,0 +1,13 @@
+package bnb.cigpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CigppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
