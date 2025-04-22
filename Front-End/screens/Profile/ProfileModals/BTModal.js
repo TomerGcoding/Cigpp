00895230885db53ac25169a3f66a3e29ee247dff
Tomer Geva <gevatomer66@gmail.com>
2025-04-22 14:26:42 +0300

    last commit on branch, bluetooth irrelevant for now

--- Front-End/screens/Profile/ProfileModals/BTModal.js
+++ Front-End/screens/Profile/ProfileModals/BTModal.js
@@ -1,15 +1,3 @@
-// import { BleManager } from "react-native-ble-plx";
-
-// const manager = new BleManager();
-
-// manager.startDeviceScan(null, null, (error, device) => {
-//   if (error) {
-//     console.error(error);
-//     return;
-//   }
-
-//   console.log("Found device:", device.name);
-// });
 const BTModal = ({ navigation }) => {
   return (
     <View>
