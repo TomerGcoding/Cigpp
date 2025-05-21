d8eb8fbb26de208eb79ebad6950395625d25fe57
Tomer Geva <gevatomer66@gmail.com>
2025-05-21 19:55:24 +0300

    removed all embedded code

--- Embedded/ble_app_uart/pca10040/s132/ses/Output/Debug/Obj/ble_app_uart_pca10040_s132/SEGGER_RTT_Syscalls_SES.d
+++
@@ -1,3 +0,0 @@
-Output/Debug/Obj/ble_app_uart_pca10040_s132/SEGGER_RTT_Syscalls_SES.o: \
- C\:\nrf_sdk\external\segger_rtt\SEGGER_RTT_Syscalls_SES.c \
- ../config/sdk_config.h
--- Embedded/ble_app_uart/pca10040/s132/ses/Output/Debug/Obj/ble_app_uart_pca10040_s132/utf.d
+++
@@ -1,6 +0,0 @@
-Output/Debug/Obj/ble_app_uart_pca10040_s132/utf.o: \
- C\:\nrf_sdk\external\utf_converter\utf.c \
- C\:\nrf_sdk\external\utf_converter\utf.h \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stddef.h \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/__crossworks.h \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stdint.h
